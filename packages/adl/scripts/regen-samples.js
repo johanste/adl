66a8ca1bec1fd0fea3a8f8f280cd925e101fdf60
Timothee Guerin <tiguerin@microsoft.com>
2021-02-11 14:23:25 -0800

    Add body time sample

--- packages/adl/scripts/regen-samples.js
+++ packages/adl/scripts/regen-samples.js
@@ -13,7 +13,8 @@ const sampleFolders = [
   "confluent",
   "nullable",
   "testserver/media-types",
-  "testserver/body-boolean"
+  "testserver/body-boolean",
+  "testserver/body-time",
 ];
 
 function resolvePath(...parts) {
